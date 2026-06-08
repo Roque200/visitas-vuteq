62c7bbb6cf1ef19336372b50fdfcca78ef967438
Roque200 <perez.roque.jesus.1amat@gmail.com>
2026-06-08 17:21:02 -0600

    Fix empresa en entradas y salidas

--- api/VisitasAPI/Controllers/EntradasController.cs
+++ api/VisitasAPI/Controllers/EntradasController.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Npgsql;
 using VisitasAPI.Services;
-
 namespace VisitasAPI.Controllers
 {
     [ApiController]
@@ -9,29 +8,19 @@ namespace VisitasAPI.Controllers
     public class EntradasController : ControllerBase
     {
         private readonly DbService _db;
-
-        public EntradasController(DbService db)
-        {
-            _db = db;
-        }
-
+        public EntradasController(DbService db) { _db = db; }
         [HttpGet]
         public async Task<IActionResult> GetEntradas()
         {
             using var con = _db.GetConnection();
             await con.OpenAsync();
-            string query = @"SELECT es.id, es.visita_id, es.fecha_entrada, es.fecha_salida, es.estatus,
-                            v.nombre_visitante, v.persona_visitar, v.empresa
-                            FROM entradas_salidas es
-                            INNER JOIN visitas v ON es.visita_id = v.id
-                            ORDER BY es.id DESC";
+            string query = "SELECT es.id, es.visita_id, es.fecha_entrada, es.fecha_salida, es.estatus, v.nombre_visitante, v.persona_visitar, v.empresa FROM entradas_salidas es INNER JOIN visitas v ON es.visita_id = v.id ORDER BY es.id DESC";
             using var cmd = new NpgsqlCommand(query, con);
             using var reader = await cmd.ExecuteReaderAsync();
             var entradas = new List<object>();
             while (await reader.ReadAsync())
             {
-                entradas.Add(new
-                {
+                entradas.Add(new {
                     id = reader["id"],
                     visitaId = reader["visita_id"],
                     fechaEntrada = reader["fecha_entrada"] == DBNull.Value ? null : Convert.ToDateTime(reader["fecha_entrada"]).ToString("dd/MM/yyyy HH:mm"),
@@ -44,7 +33,6 @@ namespace VisitasAPI.Controllers
             }
             return Ok(entradas);
         }
-
         [HttpPut("{id}/salida")]
         public async Task<IActionResult> DarSalida(int id)
         {
